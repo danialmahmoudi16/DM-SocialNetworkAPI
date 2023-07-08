d989f94617e552ecf11641e932237f3306891c8c
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-07-08 13:16:42 -0400

    updated userRoutes.js file

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// retrieve all users and create a user
+router.route('/').get(getUsers).post(createUser);
+
+// retrieve a single user, update a user, and delete a user
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// add and remove a friend
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
